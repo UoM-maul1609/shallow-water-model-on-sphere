dd0f5f93e50554aa933eff6b1ac8f637d683baca
Petra Fisher <petra.fshr@gmail.com>
2024-04-23 11:55:19 +1200

    Updating the README and adding u velocity to animate_output graphs

---
+++ README.md
@@ -0,0 +1,101 @@
+## 1. Getting started
+Clone the repository
+```bash
+git clone https://github.com/petrafisher-code/ShallowWaterModelCode.git
+```
+
+Move into the repository
+```bash
+cd ShallowWaterModelCode
+```
+
+<br>
+
+## 2. Compilation and running the code
+
+You can either compile and run using [Docker](https://www.docker.com/get-started/) or directly on your machine.
+Docker is recommended. 
+
+### Running using Docker
+Install docker on your machine. Open docker, go back to the code/your IDE and run
+the following command in terminal
+
+```bash
+docker build -t fortran_code ./ --progress=plain
+```
+
+Once complete, run the container
+```bash
+docker run fortran_code:latest
+```
+
+In the docker application under "Containers", copy the latest container ID to clipboard.
+
+In another terminal (or after stopping the container) copy the output file to your local folder
+```bash
+docker cp [CONTAINER_ID]:/tmp/output.nc /path/to/your/tests/directory
+```
+
+### Running without Docker
+
+You will need to install NETCDF on your system and note the directories where the 
+library and module reside. 
+
+In the Makefile change the variables NETCDF_FOR, NETCDF_C and NETCDF_LIB to suit 
+your system. Sometimes when compiling NETCDF you may have to install both the 
+fortran and c versions. If you do not need to do this, set NETCDF_FOR and NETCDF_C
+equal to the same value. 
+
+If you install both c and fortran versions, set NETCDF_LIB to -lnetcdff
+If you only need to install one version, set NETCDF_LIB to lnetcdf
+Finally, once netcdf is installed and the make file is edited, type "make" to compile
+the code
+
+Note, these variables are commented out in the Makefile. You may set them as environment
+variables in your .bashrc, or .bash_profile and the Makefile will pick them up.
+
+Type the following to run the code on a single processor
+```bash
+./main.exe ../config/namelist.in
+```
+To run the code with multiple processors as an MPI job
+```bash
+mpiexec -n 32 ./main.exe ../config/namelist.in
+```
+
+<br>
+
+## 3. Animating results
+There a python files provided to animate the resulting output.nc netCDF file. 
+Move to the python directory,
+```bash
+cd src/python
+```
+
+Run the python animate_output.py file
+```bash
+python animate_output.py
+```
+
+This will produce frames in the ShallowWaterModelCode/output/frames directory
+and animations in the ShallowWaterModelCode/output/animations directory.
+(If you have already produced animations, the terminal will ask you whether you
+want to overwrite the previous animations.)
+
+<br>
+
+
+## 4. Doxygen pages
+Self generating documentation (from the source code) can be created by typing
+"doxygen fortran.dxg" at the command line.
+
+You can then view the html pages by opening the file at "doxygen/html/index.html"
+
+<br>
+
+## 5. Contributing development
+At the moment the code is under private repo with no development branches. Contact 
+the code owner to contribute ideas at 
+[https://github.com/UoM-maul1609/shallow-water-model-on-sphere](https://github.com/UoM-maul1609/shallow-water-model-on-sphere).
+
+<br>
--- src/python/animate_output.py
+++ src/python/animate_output.py
@@ -21,6 +21,7 @@ lats=nc.variables['theta'][:]
 vort=nc.variables['vort'][:]
 h=nc.variables['h'][:]
 v=nc.variables['v'][:]
+u=nc.variables['u'][:]
 time=nc.variables['time'][:]
 
 if not os.path.exists('../../output/frames'):
@@ -33,8 +34,8 @@ for it1 in range(3,len(time)+1,4):
     it=it1-1
     # if iter1==0:
     f=plt.figure()
-    ax1=f.add_subplot(131)
 
+    ax1=f.add_subplot(141)
     # set up orthographic map projection with 
     # perspective of satellite looking down at 50N, 100W.
     # use low resolution coastlines.
@@ -48,18 +49,16 @@ for it1 in range(3,len(time)+1,4):
     x,y = map(lo*180./np.pi, la*180./np.pi)
     # contour data over the map.
     #cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
-    cs1 = map.pcolor(x,y,h[it,:,:],cmap='jet', shading='auto')
+    cs1 = map.pcolor(x,y,h[it,:,:],cmap='jet', shading='auto', vmin=59000, vmax=64000)
     cbar=map.colorbar(location='bottom')
     
     cbar.ax.set_xticks(cbar.ax.get_xticks())
     cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation='vertical')
     cbar.set_label('h, m')
-    ax1.set_title('Height at t=' + '{0:.2f}'.format(time[it]/(86400)) +' days')
+    ax1.set_title('Height at \n t=' + '{0:.2f}'.format(time[it]/(86400)) +' days')
 
 
-    ax2=f.add_subplot(132)
-
-
+    ax2=f.add_subplot(142)
     # set up orthographic map projection with 
     # perspective of satellite looking down at 50N, 100W.
     # use low resolution coastlines.
@@ -71,16 +70,15 @@ for it1 in range(3,len(time)+1,4):
 
     # contour data over the map.
     #cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
-    cs2 = map.pcolor(x,y,vort[it,:,:],cmap='jet',shading='auto')
+    cs2 = map.pcolor(x,y,vort[it,:,:],cmap='jet',shading='auto', vmin=-0.00008, vmax=0.00008)
     cbar=map.colorbar(location='bottom')
     cbar.ax.set_xticks(cbar.ax.get_xticks())
     cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation='vertical')
     cbar.set_label('$\zeta$, s$^{-1}$')
     ax2.set_title('Vorticity')
 
-    ax3=f.add_subplot(133)
 
-
+    ax3=f.add_subplot(143)
     # set up orthographic map projection with 
     # perspective of satellite looking down at 50N, 100W.
     # use low resolution coastlines.
@@ -92,10 +90,28 @@ for it1 in range(3,len(time)+1,4):
 
     # contour data over the map.
     #cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
-    cs3 = map.pcolor(x,y,v[it,:,:],cmap='jet',shading='auto')
+    cs3 = map.pcolor(x,y,v[it,:,:],cmap='jet',shading='auto', vmin=-7, vmax=7)
     cbar=map.colorbar(location='bottom')
     cbar.set_label('v, m s$^{-1}$')
     ax3.set_title('v')
+
+
+    ax4=f.add_subplot(144)
+    # set up orthographic map projection with 
+    # perspective of satellite looking down at 50N, 100W.
+    # use low resolution coastlines.
+    map = Basemap(satellite_height=3000000,projection='nsper',lat_0=90,lon_0=-100,resolution='l')
+    # draw the edge of the map projection region (the projection limb)
+    # draw lat/lon grid lines every 30 degrees.
+    map.drawmeridians(np.arange(0,360,30))
+    map.drawparallels(np.arange(-90,90,30))
+
+    # contour data over the map.
+    #cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
+    cs4 = map.pcolor(x,y,u[it,:,:],cmap='jet',shading='auto', vmin=-5, vmax=60)
+    cbar=map.colorbar(location='bottom')
+    cbar.set_label('u, m s$^{-1}$')
+    ax4.set_title('u')
 
     iter1 += 1
     plt.savefig('../../output/frames/frame%03d.png' % iter1,format='png', dpi=300) 
