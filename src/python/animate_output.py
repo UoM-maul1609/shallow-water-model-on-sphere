df54a6f8eb96a29d5e4ba733f2af358ec7353f86
Petra Fisher <73986146+petrafisher-code@users.noreply.github.com>
2024-05-09 18:59:04 +1200

    May 9th config and postprocessing update (#5)
    * Changing config values and reducing python font size
    
    * Changing jet latitude and making height colour map red
    
    * Update ticker import statement

--- src/python/animate_output.py
+++ src/python/animate_output.py
@@ -13,6 +13,7 @@ import warnings
 
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 
 from create_map import create_map_func
 
@@ -43,60 +44,72 @@ if not os.path.exists("../../output/animations"):
 ITER = 0
 for it1 in range(3, len(time) + 1, 4):
     it = it1 - 1
-    # if ITER==0:
     f = plt.figure()
 
     ax1 = f.add_subplot(141)
     # set up orthographic map projection
     x, y, basemap = create_map_func(lons, lats)
-    # contour data over the basemap.
-    # cs = basemap.contour(x,y,wave+mean,15,linewidths=1.5)
+    # contour data over the basemap
     cs1 = basemap.pcolor(
-        x, y, h[it, :, :], cmap="jet", shading="auto", vmin=59000, vmax=64000
+        x, y, h[it, :, :], cmap="jet", shading="auto", vmin=60000, vmax=64000
     )
     cbar = basemap.colorbar(location="bottom")
     cbar.ax.set_xticks(cbar.ax.get_xticks())
-    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="vertical")
-    cbar.set_label("h, m")
-    ax1.set_title(f"Height at \n t={time[it]/86400:.2f} days")
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="horizontal", fontsize=8)
+    # Format colorbar tick labels using scientific notation
+    formatter = ticker.ScalarFormatter(useMathText=True)
+    formatter.set_powerlimits((-3, 3))  # Adjust the limits as needed
+    cbar.ax.xaxis.set_major_formatter(formatter)
+    power_text = cbar.ax.xaxis.get_offset_text()
+    power_text.set_size(8)
+    # Set label and title
+    cbar.set_label("h, m", fontsize=8)
+    ax1.set_title("Height", fontsize=8)
 
     ax2 = f.add_subplot(142)
     # set up orthographic map projection
     x, y, basemap = create_map_func(lons, lats)
     # contour data over the basemap.
-    # cs = basemap.contour(x,y,wave+mean,15,linewidths=1.5)
     cs2 = basemap.pcolor(
-        x, y, vort[it, :, :], cmap="jet", shading="auto", vmin=-0.00008, vmax=0.00008
+        x, y, vort[it, :, :], cmap="jet", shading="auto", vmin=-0.00005, vmax=0.00005
     )
     cbar = basemap.colorbar(location="bottom")
     cbar.ax.set_xticks(cbar.ax.get_xticks())
-    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="vertical")
-    cbar.set_label("$\\zeta$, s$^{-1}$")
-    ax2.set_title("Vorticity")
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="horizontal", fontsize=8)
+    # Format colorbar tick labels using scientific notation
+    formatter = ticker.ScalarFormatter(useMathText=True)
+    formatter.set_powerlimits((-3, 3))  # Adjust the limits as needed
+    cbar.ax.xaxis.set_major_formatter(formatter)
+    power_text = cbar.ax.xaxis.get_offset_text()
+    power_text.set_size(8)
+    # Set label and title
+    cbar.set_label("$\\zeta$, s$^{-1}$", fontsize=8)
+    ax2.set_title("Vorticity", fontsize=8)
 
     ax3 = f.add_subplot(143)
     # set up orthographic map projection
     x, y, basemap = create_map_func(lons, lats)
     # contour data over the basemap.
-    # cs = basemap.contour(x,y,wave+mean,15,linewidths=1.5)
     cs3 = basemap.pcolor(x, y, v[it, :, :], cmap="jet", shading="auto", vmin=-7, vmax=7)
     cbar = basemap.colorbar(location="bottom")
-    cbar.set_label("v, m s$^{-1}$")
-    ax3.set_title("v")
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="horizontal", fontsize=8)
+    cbar.set_label("v, m s$^{-1}$", fontsize=8)
+    ax3.set_title("v", fontsize=8)
 
     ax4 = f.add_subplot(144)
     # set up orthographic map projection
     x, y, basemap = create_map_func(lons, lats)
-    # contour data over the basemap.
-    # cs = basemap.contour(x,y,wave+mean,15,linewidths=1.5)
+    # contour data over the basemap
     cs4 = basemap.pcolor(
         x, y, u[it, :, :], cmap="jet", shading="auto", vmin=-5, vmax=60
     )
     cbar = basemap.colorbar(location="bottom")
-    cbar.set_label("u, m s$^{-1}$")
-    ax4.set_title("u")
+    cbar.ax.set_xticklabels(cbar.ax.get_xticklabels(), rotation="horizontal", fontsize=8)
+    cbar.set_label("u, m s$^{-1}$", fontsize=8)
+    ax4.set_title("u", fontsize=8)
 
     ITER += 1
+    plt.suptitle(f"t={time[it]/86400:.2f} days", fontsize=8, y=0.25)
     plt.savefig(f"../../output/frames/frame{ITER:03d}.png", format="png", dpi=300)
 
 os.system(
